6698e6a5186b82b60b88930ee670ccc3f213fae3
alyssabmcnatt <6654@holbertonstudents.com>
2023-12-21 14:27:15 -0800

    idc

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -5,6 +5,8 @@
  * pre-order traversal
  * @tree: a pointer to the root node of the tree to traverse
  * @func: a pointer to a function to call for each node
+ * Description: The value in the node must be passed as a
+ * parameter to this function
  */
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
