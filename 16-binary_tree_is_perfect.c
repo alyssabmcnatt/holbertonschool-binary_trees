e0bd89be47447cab9a687a9244a076d2bfd50114
alyssabmcnatt <6654@holbertonstudents.com>
2023-12-21 15:06:14 -0800

    idc

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,9 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: a pointer to the root node of the tree to check
+ * Return: if tree is NULL, return 0
+ */
+
+
