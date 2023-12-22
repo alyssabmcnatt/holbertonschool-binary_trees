27ac6f30b6e75fad5b697a0d3e5dd463d48fc5e8
alyssabmcnatt <6654@holbertonstudents.com>
2023-12-21 20:44:32 -0800

    binary trees

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -4,11 +4,13 @@
  * binary_tree_leaves - counts the leaves in a binary tree
  * @tree: a pointer to the root node of the tree to count the
  * number of leaves
- * Return: if tree is NULL, return 0
+ * Return: the number of leaves, or if tree is NULL, return 0
  */
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t leaves = 0;
+
 	if (tree)
 	{
 		leaves += (!tree->left && !tree->right) ? 1 : 0;
